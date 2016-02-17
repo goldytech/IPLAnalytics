9fd62241b6c2633008af2d2d63d35568e057fb0e
Muhammad Afzal <afzal.qureshi@outlook.com>
2016-02-17 19:13:29 +0530

    Index and Layout view is ready

---
+++ src/IPLAnalytics.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IPLAnalytics.Web.Controllers
+{
+    using IPLAnalytics.Web.Services;
+
+    using Microsoft.AspNet.Mvc;
+
+    public class HomeController : Controller
+    {
+        private readonly IViewModelService viewModelService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="viewModelService">
+        /// The view model service.
+        /// </param>
+        public HomeController(IViewModelService viewModelService)
+        {
+            this.viewModelService = viewModelService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "IPL Analytics App2";
+            return View(this.viewModelService.GetDashboardViewModel());
+        }
+    }
+}
